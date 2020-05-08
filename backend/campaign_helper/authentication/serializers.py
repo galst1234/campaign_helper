73ea2e0608ffdb1fcb676a9e90a3d52682cb6bef
Gal <galst1234@gmail.com>
2020-05-08 20:20:35 +0300

    Add password and email to user serializer and allow user creation.

--- backend/campaign_helper/authentication/serializers.py
+++ backend/campaign_helper/authentication/serializers.py
@@ -5,4 +5,15 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username',)
+        fields = ('id', 'username', 'password', 'email')
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data: dict) -> User:
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
