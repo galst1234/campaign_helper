49f9c4d18700e1152c33810d9851ba48c078a92d
Gal <galst1234@gmail.com>
2020-05-08 16:29:32 +0300

    Add persistent login.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Layout} from 'antd';
 import './App.css';
 import {Router} from "react-router-dom";
@@ -8,6 +8,8 @@ import {HeaderMenu} from "./Components/Layout/Header";
 import {LoginModal} from "./Components/Auth/LoginModal";
 import {User} from "./backend_api/models";
 import {MainSwitch} from './Components/Layout/Navigation';
+import {JwtApi, localStorageAccessToken, localStorageRefreshToken} from "./ApiClient/ApiClient";
+
 
 const history = createBrowserHistory();
 
@@ -17,6 +19,24 @@ export const UserContext = React.createContext<User | undefined>(undefined);
 function App() {
     const [showLoginModal, setShowLoginModal] = useState<boolean>(false);
     const [user, setUser] = useState<User | undefined>(undefined);
+    const API = new JwtApi();
+
+    /**
+     * On load, check if there is a saved token and try to get the logged-in user using it.
+     * On fail clear the saved data.
+     */
+    useEffect(() => {
+        const accessToken = localStorage.getItem(localStorageAccessToken);
+        if (accessToken) {
+            API.myInfoUser().then(userData => {
+                setUser(userData);
+            }, () => {
+                localStorage.removeItem(localStorageAccessToken);
+                localStorage.removeItem(localStorageRefreshToken);
+            });
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <UserContext.Provider value={user}>
